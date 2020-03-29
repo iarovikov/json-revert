eff83e0d05f7747ea1e17644f0a5e5b62cf6c8e4
Vadim Yarovikov <vadim.yarovikov@gmail.com>
2020-03-29 13:22:42 +0200

    Rename controller

---
+++ src/JsonRevert.WebAPI/Controllers/RevertController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JsonRevert.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RevertController : ControllerBase
+    {
+       // POST api/revert
+        [HttpPost]
+        public void Post([FromBody] string json)
+        {
+        }
+    }
+}
