40141f858c1d60b5d78e54c71627fd5c21c3532b
Vadim Yarovikov <vadim.yarovikov@gmail.com>
2020-03-29 14:08:52 +0200

    Add failed test

---
+++ src/JsonRevert.UnitTests/JsonReverterTests.cs
@@ -0,0 +1,23 @@
+using System;
+using JsonRevert.WebAPI.BusinessLogic;
+using Xunit;
+
+namespace JsonRevert.UnitTests
+{
+    public class JsonReverterTests
+    {
+        [Theory]
+        [InlineData("{A:1, B:1, C:1, D:1, E:1}", "{E:1, D:1, C:1, B:1, A:1}")]
+        public void ShouldRevertInputJson(string input, string expected)
+        {
+            // Arrange
+            var sut = new JsonReverter();
+            
+            // Act
+            var actual = sut.RevertJson(input);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
--- src/JsonRevert.WebAPI/Startup.cs
+++ src/JsonRevert.WebAPI/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using JsonRevert.WebAPI.BusinessLogic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,6 +33,8 @@ namespace JsonRevert.WebAPI
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "JsonRevert API", Version = "v1" });
             });
+
+            services.AddTransient<IJsonReverter, JsonReverter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
